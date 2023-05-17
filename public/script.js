fe9e0df6edcba9dcefe9c42cb91c0f0790ed7e5e
Muhammad Fadll <fadeljourney@gmail.com>
2023-05-17 21:57:24 +0700

    update: directory file and functions

--- public/script.js
+++ public/script.js
@@ -3,10 +3,6 @@ const inputTxt = document.getElementById("myInput");
 const todoList = document.querySelector("ul.todo-list");
 const totalItems = document.getElementById("item-length");
 const modeButton = document.getElementById("mode-btn");
-// const todoItems = todoList.childNodes;
-// const todoItems = document.querySelectorAll("li.todo-item");
-
-// console.log(todoItems)
 
 const allBtn = document.getElementById("all-btn");
 const activeBtn = document.getElementById("active-btn");
@@ -15,15 +11,32 @@ const clearComplt = document.getElementById("clear-btn");
 
 
 
-let data = ["workout", "sarapan", "kuliah"];
+let data = [
+  { name: "workout", check: false },
+  { name: "sarapan", check: false },
+  { name: "kuliah", check: false },
 
-const listTemplate = (item, index) => {
-  return `<li class="todo-item group dark:border-darkTheme-700 px-4 h-10 border-b-2 flex items-center relative ">
-          <button onclick={checked(${index})} class="check-border p-1 text-align bg-transparent mr-2 w-5 h-5 rounded-full border-2 border-lightTheme-400">
+
+]
+
+const listTemplate = ({ name, check }, id) => {
+  if (check) {
+    return `<li   class="todo-item group dark:border-darkTheme-700 px-4 h-10 border-b-2 flex items-center relative ">
+              <button onclick={checked(event)}   class="check-border bg-check border-transparent  p-1 text-align bg-transparent mr-2 w-5 h-5 rounded-full border-2 border-lightTheme-400">
+              <img src="../src/images/icon-check.svg" class="checked " />
+              </button>                
+            <span onclick={checked(event)}  class="text text-lightTheme-300 line-through text-${id} cursor-pointer w-full ">${name}</span>
+            <button onclick={deleteItem(${id})} class="close-button opacity-0 cursor-pointer group-hover:opacity-100 ease-out duration-500 w-5 h-5 absolute top-3 right-4">
+            <img src="../src/images/icon-cross.svg" class="close ml-2 border-lightTheme-400 w-full "/>
+            </button>
+            </li>`
+  }
+  return `<li   class="todo-item group dark:border-darkTheme-700 px-4 h-10 border-b-2 flex items-center relative ">
+          <button onclick={checked(event)}   class="check-border p-1 text-align bg-transparent mr-2 w-5 h-5 rounded-full border-2 border-lightTheme-400">
           <img src="../src/images/icon-check.svg" class="checked opacity-0" />
           </button>                
-        <span class="text text-${index + 1} cursor-pointer w-full">${item}</span>
-        <button onclick={deleteItem(${item})} class="close-button opacity-0 cursor-pointer group-hover:opacity-100 ease-out duration-500 w-5 h-5 absolute top-3 right-4">
+        <span onclick={checked(event)}  class="text text-${id} cursor-pointer w-full">${name}</span>
+        <button onclick={deleteItem(${id})} class="close-button opacity-0 cursor-pointer group-hover:opacity-100 ease-out duration-500 w-5 h-5 absolute top-3 right-4">
         <img src="../src/images/icon-cross.svg" class="close ml-2 border-lightTheme-400 w-full "/>
         </button>
         </li>`
@@ -36,9 +49,9 @@ const loadAllData = () => {
   const object = localStorage.getItem('object')
   const getData = JSON.parse(object)
   todoList.innerHTML = ""
+  getData.map((item, i) => {
 
-  getData.map((item, index) => {
-    const newItem = listTemplate(item, index);
+    const newItem = listTemplate(item, i)
     todoList.innerHTML += newItem;
   })
 
@@ -46,27 +59,65 @@ const loadAllData = () => {
 }
 
 // add new data
-inputTxt.addEventListener("keyup", (event) => {
-  if (event.key == "Enter") {
-    data.push(inputTxt.value)
+const addItem = (e) => {
+  if (e.key == "Enter") {
+    data.push({ id: data.length, name: inputTxt.value, check: false })
     inputTxt.value = ""
     loadAllData()
   }
+}
 
-})
+inputTxt.addEventListener("keyup", addItem)
 
-const checked = (id) => {
-  console.log(id)
 
+// check data
+const checked = (e) => {
+  data.map(item => {
+    if (e.target.innerHTML == item.name) {
+      item.check = !item.check
+    }
+  })
+  loadAllData()
 }
 
 // delete data
 const deleteItem = (index) => {
-  console.log(index)
-  // data = data.filter((item, i) => i !== index)
+
+  data = data.filter((item, i) => i !== index)
   loadAllData()
 }
 
+// item active
+activeBtn.addEventListener('click', () => {
+  let count = 0
+  data.map(item => {
+    if (item.check == false) count++
+  })
+  totalItems.innerHTML = `${count} items length`;
+  count = 0
+})
+
+// show items length
+allBtn.addEventListener('click', () => {
+  loadAllData()
+})
+
+// show items length completed
+completedBtn.addEventListener('click', () => {
+  let count = 0
+  data.map(item => {
+    if (item.check == true) count++
+  })
+  totalItems.innerHTML = `${count} items length`;
+  count = 0
+})
+
+
+// clear completed
+clearComplt.addEventListener('click', () => {
+  data = data.filter(item => item.check !== true)
+  loadAllData()
+})
 
 
 // change mode
@@ -86,3 +137,5 @@ const changeMode = () => {
 
 
 modeButton.addEventListener("click", changeMode);
+
+
